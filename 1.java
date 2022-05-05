0818268a2728af32ea82d912bfd407f216d94974
rajesh70366 <rajesh.raja70366@gmail.com>
2022-05-05 09:29:02 +0530

    a

---
+++ 1.java
@@ -0,0 +1,9 @@
+firstly im install gitub application and create gitup account
+and also create repository in gitub account
+mainly i will face some of new commands in gitub for init,add,commit,change branch matser to main,and finally push command 
+i will try to some commands in porpose of push up files local to gitub repositary
+im facing so many errors in different stages example main error is push command raise the error no such files and directory
+finally end of the almost all completely using  https and ssh commands to push files to repository
+
+
+ 
